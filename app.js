4b549e83986ff248b94dc16106df4cab98d84e58
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-04 13:30:57 +0100

    adding more tests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // string length
 
 function stringLength(str) {
+    if (str.length < 1 || str.length > 10) {
+        throw new Error('string length must be between 1 and 10');
+    }
     return str.length;
 }
 
--- test.js
+++ test.js
@@ -9,4 +9,12 @@ describe('stringLength', () => {
         // assert
         expect(result).toBe(5);
     });
+    test('throws an error if the string is too short', () => {
+        // arrange
+        const arg = '';
+        // act
+        const result = () => stringLength(arg);
+        // assert
+        expect(result).toThrow();
+    });
 });
